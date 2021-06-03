ce460f71b347849388d7003859b43f229d4252f3
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2021-06-03 20:27:41 +0200

    feat(header): add styles

--- src/LitHn.js
+++ src/LitHn.js
@@ -1,11 +1,21 @@
 import { ScopedRegistryHost } from "@lit-labs/scoped-registry-mixin";
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { HnFooter } from "./hn-footer/HnFooter.js";
 import { HnHeader } from "./hn-header/HnHeader.js";
 import { Router } from "./router/index.js";
 import { routes } from "./routes.js";
 
 export class LitHn extends ScopedRegistryHost(LitElement) {
+
+	static get styles() {
+		return css`
+		:host {
+			--primary-color: #325cff;
+			--primary-color-contrast: #fff;
+		}
+		`;
+	}
+
   static get elementDefinitions() {
     return {
       "hn-footer": HnFooter,
@@ -21,15 +31,7 @@ export class LitHn extends ScopedRegistryHost(LitElement) {
   // eslint-disable-next-line class-methods-use-this
   render() {
     return html`
-      <hn-header
-        >Hacker News
-        <nav>
-          <ul>
-            <li><a href="./">news</a></li>
-            <li><a href="./ask">ask</a></li>
-          </ul>
-        </nav>
-      </hn-header>
+      <hn-header></hn-header>
       <main>${this.router.render()}</main>
       <hn-footer> Footer </hn-footer>
     `;
--- src/hn-header/HnHeader.js
+++ src/hn-header/HnHeader.js
@@ -1,9 +1,64 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { ScopedRegistryHost } from "@lit-labs/scoped-registry-mixin";
 
 export class HnHeader extends ScopedRegistryHost(LitElement) {
+
+	static get styles() {
+		return css`
+			:host {
+				display:flex;
+				align-items: center;
+				gap: 1rem;
+				padding: 0.5rem;
+				background: var(--primary-color);
+				color: var(--primary-color-contrast);
+			}
+			img {
+				flex: 0;
+				height: 3.25rem;
+    		width: 2.6rem;
+			}
+			nav {
+				flex: 1 0 auto;
+				display: flex;
+				flex-direction: column;
+				gap: 0.5rem;
+			}
+			h1 {
+				margin: 0;
+				font-size: 1.2rem;
+			}
+			ul {
+				margin: 0;
+				padding: 0;
+				list-style: none;
+				display: flex;
+				gap: 0.5rem;
+			}
+
+			li:not(:last-child)  {
+				padding-right: 0.5rem;
+				border-right: 1px solid var(--primary-color-contrast);
+			}
+
+			a {
+				color: var(--primary-color-contrast);
+				text-decoration: none;
+			}
+		`;
+	}
+
   // eslint-disable-next-line class-methods-use-this
   render() {
-    return html`<slot></slot>`;
+    return html`
+		<img src="https://lit.dev/images/flame-favicon.svg" alt="Logo Lit"/>
+		<nav>
+			<h1>Hacker News Lit</h1>
+			<ul>
+				<li><a href="./">news</a></li>
+				<li><a href="./ask">ask</a></li>
+			</ul>
+		</nav>
+			`;
   }
 }
