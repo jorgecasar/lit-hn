2079da1b40aea509da2856037c76b0d1fc8f10cb
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2021-06-10 19:27:36 +0200

    refactor: Api as controller

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-import { Task } from "@lit-labs/task";
+import { Task, initialState } from "@lit-labs/task";
 import { ReactiveElement } from "lit";
 import { apiUrl } from "./config/index.js";
 
@@ -9,11 +9,49 @@ import { apiUrl } from "./config/index.js";
  * @param {number} data.page page number
  * @returns {Task} Api task controller
  */
-export function fetchFeeds(host, { feed = "news", page = 1 }) {
-  return new Task(
-    host,
-    async ([feed, page]) =>
-      (await fetch(`${apiUrl}/${feed}/${page}.json`)).json(),
-    () => [feed, page]
-  );
+
+export class FeedsController {
+	/**
+	 *
+	 * @param {ReactiveControllerHost} host
+	 */
+	constructor(host) {
+		this.host = host;
+		this.task = this.createTask();
+	}
+
+	/**
+	 *
+	 * @returns {Task} task
+	 */
+	createTask() {
+		return new Task(
+			this.host,
+			async ([feed, page]) => {
+				if (!feed.trim()) {
+					return initialState;
+				}
+				const response = await fetch(`${apiUrl}/${feed}/${page}.json`);
+				const result = await response.json();
+				const { error } = result;
+        if (error !== undefined) {
+          throw new Error(error);
+        }
+        return result;
+			},
+			() => [
+				this.host.feed,
+				this.host.page
+			]
+		)
+	}
+
+	/**
+	 *
+	 * @param {StatusRenderer} renderFunctions render functions
+	 * @returns {any} Template
+	 */
+	render(renderFunctions) {
+    return this.task.render(renderFunctions);
+  }
 }
